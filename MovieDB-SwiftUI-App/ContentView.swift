35a6eb92e6140668f46464520af0b87209ba243d
miruoon <rillakerika@gmail.com>
2024-06-01 14:48:11 +0900

    Initial Commit

---
+++ MovieDB-SwiftUI-App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MovieDB-SwiftUI-App
+//
+//  Created by erika yoshikawa on 2024/06/01.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
